8d96005b7e0319c56b04da27623916c481978fff
Sonia <sonia.fernandez@davinci.edu.ar>
2023-05-21 22:08:06 -0300

    feat: add season class

---
+++ src/torunament_pk/Season.java
@@ -0,0 +1,43 @@
+package torunament_pk;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Season {
+    private String name;
+    private Date startDate;
+    private ArrayList<Match>matches;
+    private ArrayList<Team>teams;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public ArrayList<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(ArrayList<Match> matches) {
+        this.matches = matches;
+    }
+
+    public ArrayList<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(ArrayList<Team> teams) {
+        this.teams = teams;
+    }
+}
